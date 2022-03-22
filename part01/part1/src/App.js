2b146406da20adf32b9c9ca02a519649d6c3bf47
miked <mpwdonnelly@gmail.com>
2022-03-21 21:30:01 -0500

    updates our Hello World page

--- part01/part1/src/App.js
+++ part01/part1/src/App.js
@@ -1,11 +1,19 @@
 
 
-function App() {
+const App = () => {
+  const now = new Date()
+  const a = 10
+  const b = 20
+
   return (
     <div className="App">
         <p>Hello, World!</p>
+        <p>The time and date are now: {now.toString()}</p>
+        <p>
+          {a} plus {b} is {a+b}
+        </p>
     </div>
-  );
+  )
 }
 
 export default App;
