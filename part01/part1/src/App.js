925dc6d84a0e07a8fe3a3caa0b10fd45304c019e
miked <mpwdonnelly@gmail.com>
2022-03-21 22:20:37 -0500

    finish courseinfo exercise

--- part01/part1/src/App.js
+++ part01/part1/src/App.js
@@ -1,17 +1,20 @@
 
-
-const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-
+const Hello = (props) => {
   return (
     <div className="App">
-        <p>Hello, World!</p>
-        <p>The time and date are now: {now.toString()}</p>
-        <p>
-          {a} plus {b} is {a+b}
-        </p>
+        <p>Hello, {props.name}!</p>
+        <p>Your birthdate is {props.bday}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Buzz" bday="June 4th" />
+      <Hello name="Dolly" bday="Jan 1st" />
+      <Hello name="Roscoe P. Coltrane" bday="unknown" />
     </div>
   )
 }
