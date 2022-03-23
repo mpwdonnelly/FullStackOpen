5d93335f2636f7d4ba49459b0ca914d1b9e9fdea
miked <mpwdonnelly@gmail.com>
2022-03-22 21:13:07 -0500

    implement a refresh function

--- part01/part1c/src/App.js
+++ part01/part1c/src/App.js
@@ -1,29 +1,8 @@
-const Hello = ({ name, age }) => {
-
-  // helper function to guess birth year
-  const birthYear = () => new Date().getFullYear() - age
-
+const App = (props) => {
+  const {counter} = props
   return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-        You were probably born in {birthYear()}
-      </p>
-    </div>
+    <div>{counter}</div>
   )
-}
-
-const App = () => {
-  const name = "Peter"
-  const age = 34
-
-  return (
-    <div className="App">
-        <h1>Greetings</h1>
-        <Hello name='asdf' age={23+3223} />
-        <Hello name={name} age={age} />
-    </div>
-  );
 }
 
 export default App;
