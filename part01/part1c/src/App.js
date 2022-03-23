e47d078b2e19e301b20d4be9cd1d90314efcfaec
miked <mpwdonnelly@gmail.com>
2022-03-23 16:07:26 -0500

    final status FSO part 1d updates

--- part01/part1c/src/App.js
+++ part01/part1c/src/App.js
@@ -1,58 +1,27 @@
 import { useState } from 'react'
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
-  return (
-    <div>
-      button press history: {props.allClicks.join('-')}
-    </div>
-  )
-}
+const Display = props => <div>{props.value}</div>
 
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
   </button>
 )
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-
-  const handleLeftClick = () => {
-    console.log('left click')
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
+  const [value, setValue] = useState(10)
+  
+  const setToValue = (newValue) => () => {
+    console.log('value now', newValue)  // print the new value to console
+    setValue(newValue)
   }
-
-  const handleRightClick = () => {
-    console.log('right click')
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
-  }
-
-  const resetClick = () => {
-    console.log('reset click')
-    setRight(0)
-    setLeft(0)
-    setAll(['-'])
-  }
-
+  
   return (
     <div>
-      {left}
-      <Button handleClick={handleLeftClick} text='left' />
-      <Button handleClick={resetClick} text='reset' />
-      <Button handleClick={handleRightClick} text='right' />
-      {right}
-      <History allClicks={allClicks} />
+      {value}
+      <button onClick={setToValue(1000)}>thousand</button>
+      <button onClick={setToValue(0)}>reset</button>
+      <button onClick={setToValue(value + 1)}>increment</button>
     </div>
   )
 }
