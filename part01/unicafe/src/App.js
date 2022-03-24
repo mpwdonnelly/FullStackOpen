124221b7cb9035335f09d742523c767d0ed34241
miked <mpwdonnelly@gmail.com>
2022-03-23 22:02:45 -0500

    first pass, adds basic event handling

--- part01/unicafe/src/App.js
+++ part01/unicafe/src/App.js
@@ -1,9 +1,22 @@
 import { useState } from 'react'
 
-function App() {
+const App = () => {
+
+  const [ good, goodCount ] = useState(0)
+  const [ bad, badCount ] = useState(0)
+  const [ neutral, neutralCount ] = useState(0)
+
   return (
     <div className="App">
-
+      <h2>give feedback</h2>
+      <button onClick={() => goodCount(good + 1)}>good</button>
+      <button onClick={() => neutralCount(neutral + 1)}>neutral</button>
+      <button onClick={() => badCount(bad + 1)}>bad</button>
+      <h2>statistics</h2>
+      
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   );
 }
