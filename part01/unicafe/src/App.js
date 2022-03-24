cab0be3efd82a58dbedb98fa26fc63194b308a95
miked <mpwdonnelly@gmail.com>
2022-03-24 14:07:10 -0500

    complete unicafe step 5: extract components

--- part01/unicafe/src/App.js
+++ part01/unicafe/src/App.js
@@ -1,13 +1,32 @@
 import { useState } from 'react'
 
+
+
+const StatisticLine = ({text, value}) => {
+  return (
+    <>
+      <p>{text} {value} </p>
+    </>
+  )
+}
+
+const Button = ({onClick, text}) => {
+  return (
+    <>
+      <button onClick={onClick}>{ text }</button>
+    </>
+    
+  )
+}
+
 const Statistics = ({ bad, neutral, good, total }) => {
   if (total===0) {return (<><p>No feedback given</p></>)}
   return (
     <>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="total" value={total} />
       <p>average {((total / 3).toFixed(2))}</p>
       <p>percent positive = { (total > 0) ? (((good/total)* 100).toFixed(2)) : '0.00'} %</p>
     </>
@@ -16,17 +35,23 @@ const Statistics = ({ bad, neutral, good, total }) => {
 
 const App = () => {
 
-  const [ good, goodCount ] = useState(0)
-  const [ bad, badCount ] = useState(0)
-  const [ neutral, neutralCount ] = useState(0)
+  const [ good, setGood ] = useState(0)
+  const [ bad, setBad ] = useState(0)
+  const [ neutral, setNeutral ] = useState(0)
   const total = good + bad + neutral
+
+  const incrementGood = () => setGood(good + 1)
+  const incrementBad = () => setBad(bad + 1)
+  const incrementNeutral = () => setNeutral(neutral + 1)
 
   return (
     <div className="App">
       <h2>give feedback</h2>
-      <button onClick={() => goodCount(good + 1)}>good</button>
-      <button onClick={() => neutralCount(neutral + 1)}>neutral</button>
-      <button onClick={() => badCount(bad + 1)}>bad</button>
+
+      <Button onClick={incrementGood} text='good' />
+      <Button onClick={incrementNeutral} text='neutral' />
+      <Button onClick={incrementBad} text='bad' />
+
       <h2>statistics</h2>
       <Statistics bad={bad} good={good} neutral={neutral} total={total} />
       
