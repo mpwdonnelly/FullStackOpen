1a63f59656c4c5a29ef08ea89affcbd2336d2f67
miked <mpwdonnelly@gmail.com>
2022-03-25 13:31:21 -0500

    finish anecdotes exercise, part 1

--- part01/anecdotes/src/App.js
+++ part01/anecdotes/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 
 const Button = ({onClick, text}) => {
   return (
@@ -8,20 +8,11 @@ const Button = ({onClick, text}) => {
   )
 }
 
+
 const App = () => {
 
-  const selectAnecdote = () => {
-    // ensure that the next selection is different from the previous
-    // prevents repetition
-    let rand = -1
-    while (rand === selected || rand < 1) {
-      rand = Math.floor(Math.random() * 7)
-    }
-    setSelected(rand)
-  }
-
   const anecdotes = [
-    'If it hurts, do it more often',
+    'If it hurts, do it more often', 
     'Adding manpower to a late software project makes it later!',
     'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
     'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
@@ -30,13 +21,65 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
   ]
 
-   
+  const [votes, setVotes] = useState (new Array(anecdotes.length).fill(0))
   const [selected, setSelected] = useState(0)
 
+
+  const selectAnecdote = () => {
+    // ensure that the next selection is different from the previous
+    // prevents repetition
+    let rand = -1
+    while (rand === selected || rand < 0) {
+      rand = Math.floor(Math.random() * 7)
+    }
+    setSelected(rand)
+  }
+
+  const addVote = () => {
+    // make a copy of the current votes array
+    const votesCopy = votes
+    //add one to the selected item because it just got one votes
+    votesCopy[selected] += 1
+    // set the main array to be equal to updated version
+    setVotes(votesCopy)
+
+  }
+
+  const Display = () => {
+
+    return (
+      <>
+        <h2>Anecdote of the day</h2>
+        <p>{anecdotes[selected]}</p>
+        <p>has {votes[selected]} votes</p>
+      </>
+    )
+  }
+
+  const Winner = () => {
+
+    const winning = Math.max(...votes)
+    const winner = votes.findIndex(element => element == winning)
+
+    return (
+      <>
+        <h3>Anecdote with the most votes</h3>
+        <p>{anecdotes[winner]}</p>
+        <p>has {votes[winner]} votes</p>
+      </>
+    )
+  }
+
+
   return (
+
     <div>
-      <p>{anecdotes[selected]}</p>
+      <Display />
+      <Button onClick={addVote} text="vote" />
       <Button onClick={selectAnecdote} text="next anecdote" />
+      <Winner />
+
+
     </div>
   )
 }
