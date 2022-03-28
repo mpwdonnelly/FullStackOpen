323db972e136498f4e1d25e82e8febeb746f857b
miked <mpwdonnelly@gmail.com>
2022-03-28 13:57:18 -0500

    finishes rendering section part 2.1

--- part02/rendering/src/App.js
+++ part02/rendering/src/App.js
@@ -1,18 +1,38 @@
+import Note from './components/Note'
 
-
-const App = (props) => {
-  const { notes } = props
+const App = ({ notes }) => {
 
   return (
     <div>
       <h1>Notes</h1>
+      <h3>rendered by hard coding</h3>
       <ul>
         <li>{notes[0].content}</li>
         <li>{notes[1].content}</li>
         <li>{notes[2].content}</li>
       </ul>
+
+      <h3>rendered using map</h3>
+      <ul>
+        {notes.map(note => 
+          <li key={note.id}>
+            {note.content}
+          </li>
+        )}
+      </ul>
+      <h3>rendered using destructuring</h3>
+      <ul>
+        {notes.map(note => <li key={note.id}>{note.content}</li>)}
+      </ul>
+      <h3>rendered using a separate component</h3>
+      <ul>
+          {notes.map(note => <Note key={note.id} note={note}/>)}
+
+      </ul>
     </div>
   )
+
 }
+
 
 export default App
